ef0d24c1490c1d63e536a098f8e705951a7247c0
sim511777 <sim51177@gmail.com>
2020-08-04 17:30:17 +0900

    DoJob리턴값 수정

--- AsyncAwait/Form1.cs
+++ AsyncAwait/Form1.cs
@@ -15,24 +15,25 @@ namespace AsyncAwait {
             InitializeComponent();
         }
 
-        private DateTime DoJob() {
+        private string DoJob(string jobName) {
+            var st = DateTime.Now;
             for (int i = 0; i < 5; i++) {
                 Thread.Sleep(1000);
             }
-            var finishTime = DateTime.Now;
-            return finishTime;
+            var dt = DateTime.Now - st;
+            return $"{jobName} - {dt.TotalSeconds:f2} sec";
         }
 
         private void btnSync_Click(object sender, EventArgs e) {
-            var finishTime = DoJob();
-            this.Text = $"Sync  Job finished : {finishTime}";
+            var jobResult = DoJob("sync");
+            this.Text = jobResult;
         }
 
         private async void btnAsync_Click(object sender, EventArgs e) {
-            var finishTime = await Task.Run(() => DoJob());
+            var jobResult = await Task.Run(() => DoJob("async"));
             // await를 만나면 작업을 비동기 적으로 실행하고 제어권을 호출자에게 넘겨준다.
             // 비동기 작업 완료되면 결과값을 리턴하고 제어권은 가져와서 이후의 코드를 실행한다.
-            this.Text = $"Async Job finished : {finishTime}";
+            this.Text = jobResult;
         }
     }
 }
